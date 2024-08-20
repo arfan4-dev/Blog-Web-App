991ba64d488ce20f8fdd479f68c1e5b2b1ae533a
MUHAMMAD ARFAN <106101890+arfan4-dev@users.noreply.github.com>
2024-08-21 03:52:56 +0500

    set cors

--- server/src/app.js
+++ server/src/app.js
@@ -3,14 +3,7 @@ import cors from 'cors'
 import cookieParser from 'cookie-parser';
 
 const app = express();
-app.use(cors(
-    {
-        origin: 'https://blog-web-app-sosz.vercel.app', // Replace with your client's URL
-        methods: ['GET', 'POST', 'PUT', 'DELETE'], // Allowed methods
-        allowedHeaders: ['Content-Type', 'Authorization'], // Allowed headers
-        credentials: true // Allow cookies and other credentials
-    }
-));
+app.use(cors());
 app.use(express.json({ limit: "16kb" }));
 app.use(urlencoded({ extended: true })); // this is use for encode url request
 app.use(cookieParser());
